df382b43cd1148b88a051cd673d10f170c71f207
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2025-03-10 12:56:49 +0100

    Create Order model

---
+++ GotorzProject/GotorzProject/Model/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace GotorzProject.Model
+{
+    public class Order
+    {
+        public enum OrderStatuses
+        {
+            Created,
+            Processing,
+            Finalized
+        }
+        public string PaymentMethod {  get; set; }
+        public double Price { get; set; }
+        public OrderStatuses OrderStatus { get; set; }
+
+        public Order(string paymentMethod, double price, OrderStatuses orderStatus)
+        {
+            PaymentMethod = paymentMethod;
+            Price = price;
+            OrderStatus = orderStatus;
+        }
+    }
+}
