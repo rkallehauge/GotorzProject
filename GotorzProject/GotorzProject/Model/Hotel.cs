0f05e14ba3cef61e21073ff5eed1dd5042a8dd02
Esben Ejdesgaard <Esbenejdesgaard@gmail.com>
2025-03-10 11:41:54 +0100

    Hotel base

---
+++ GotorzProject/GotorzProject/Model/Hotel.cs
@@ -0,0 +1,22 @@
+﻿namespace GotorzProject.Model
+{
+    public class Hotel
+    {
+        //todo: switch with paymentmodel
+
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int Stars { get; set; }
+        public int Price { get; set; }
+        public int NumberOfRooms { get; set; }
+
+        public Hotel(string name, string adress, int stars, int price, int numberOfRooms)
+        {
+            Name = name;
+            Adress = adress;
+            Stars = stars;
+            Price = price;
+            NumberOfRooms = numberOfRooms;
+        }
+    }
+}
