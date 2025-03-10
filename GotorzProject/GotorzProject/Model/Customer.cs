9c1d68b6ffece26c0944e047d276ec082fbc660c
Lars Jensen <laje63255@edu.ucl.dk>
2025-03-10 11:14:08 +0100

    CustomerEmployeeModel

---
+++ GotorzProject/GotorzProject/Model/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace GotorzProject.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string TelephoneNumber { get; set; }
+        public int ID { get; set; } // todo: switch to unique username and password
+
+        public Customer(string firstName, string lastName, string address, string postalCode, string country, string telephoneNumber, int iD)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            PostalCode = postalCode;
+            Country = country;
+            TelephoneNumber = telephoneNumber;
+            ID = iD;
+        }
+
+    }
+}
---
+++ GotorzProject/GotorzProject/Model/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace GotorzProject.Model
+{
+    public class Employee : Customer
+    {
+        public string Role { get; set; }
+
+        public Employee(string firstName, string lastName, string address, string postalCode, string country, string telephoneNumber, int iD, string role) : base(firstName, lastName, address, postalCode, country, telephoneNumber, iD)
+        {
+            Role = role;
+        }
+    }
+}
