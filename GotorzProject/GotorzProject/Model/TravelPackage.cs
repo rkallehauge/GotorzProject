d1220c2dd9992546687d380af68695e73d60fb9f
psytracks <maje62360@edu.ucl.dk>
2025-03-10 12:51:30 +0100

    Made TravelPackage

---
+++ GotorzProject/GotorzProject/Model/TravelPackage.cs
@@ -0,0 +1,25 @@
+﻿namespace GotorzProject.Model
+{
+    public class TravelPackage
+    {
+
+        public int PaymentID { get; set; }
+        public string DestinationCity { get; set; }
+        public string DestinationCountry { get; set; }
+        public string Hotel { get; set; }
+        public double Price { get; set; }
+        public DateTime Departure { get; set; }
+        public DateTime Return { get; set; }
+
+        public TravelPackage(int paymentID, string destinationCity, string destinationCountry, string hotel, double price, DateTime departure, DateTime @return)
+        {
+            PaymentID = paymentID;
+            DestinationCity = destinationCity;
+            DestinationCountry = destinationCountry;
+            Hotel = hotel;
+            Price = price;
+            Departure = departure;
+            Return = @return;
+        }
+    }
+}
