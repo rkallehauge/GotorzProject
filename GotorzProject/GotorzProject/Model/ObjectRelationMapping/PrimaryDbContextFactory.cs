a7112653b8f2ca112d2c23322f2a691ee69ec53c
psytracks <maje62360@edu.ucl.dk>
2025-03-10 18:08:10 +0100

    Test

--- GotorzProject/GotorzProject/Model/ObjectRelationMapping/PrimaryDbContextFactory.cs
+++ GotorzProject/GotorzProject/Model/ObjectRelationMapping/PrimaryDbContextFactory.cs
@@ -8,7 +8,7 @@ namespace GotorzProject.Model.ObjectRelationMapping
         public PrimaryDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<PrimaryDbContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Gotorz;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GoTorz;Trusted_Connection=True;MultipleActiveResultSets=true");
 
             return new PrimaryDbContext(optionsBuilder.Options);
         }
