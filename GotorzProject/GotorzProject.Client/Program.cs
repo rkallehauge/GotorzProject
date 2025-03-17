5dd76a2312c048bcfffdc29d5be900548806cff1
psytracks <maje62360@edu.ucl.dk>
2025-03-17 23:19:44 +0100

    Lavet chat vindue

--- GotorzProject/GotorzProject.Client/Program.cs
+++ GotorzProject/GotorzProject.Client/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Reflection.Metadata.Ecma335;
 using static GotorzProject.Client.Pages.Support;
+using Syncfusion.Blazor;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddSyncfusionBlazor();
 
 // todo : fix / make this better pls
 string hardcodedLocalUrl = "https://localhost:7097";
