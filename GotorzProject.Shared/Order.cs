be6d0ef10c7f1d7ec7fa66f1ba77dc67a2bcd85b
psytracks <maje62360@edu.ucl.dk>
2025-03-20 16:08:19 +0100

    Oprettet Orderhistorik

---
+++ GotorzProject.Shared/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace GotorzProject.Shared
+{
+    public class Order
+    {
+
+        public enum OrderStatuses
+        {
+            Created,
+            Processing,
+            Finalized
+        }
+        public int ID { get; set; }
+        public string? Date { get; set; }
+        public string? DateYear { get; set; }
+        public string? Destination { get; set; }
+        public string? Hotel { get; set; }
+        public double Price { get; set; }
+        public string? PaymentMethod { get; set; }
+        public OrderStatuses OrderStatus { get; set; }
+    }
+}
