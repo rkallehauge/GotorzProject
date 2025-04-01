3cff97c3bfced1e35e4dfe7d0d08c8ab28264d49
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2025-04-01 14:17:06 +0200

    attempt fixing list issue

---
+++ Shared/DataTransfer/TravelPackageDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotorzProject.Shared.DataTransfer
+{
+    public class TravelPackageDTO
+    {
+        public string DestinationCity { get; set; }
+        public string DestinationCountry { get; set; }
+        public string Hotel { get; set; }
+        public double Price { get; set; }
+        public DateTime Departure { get; set; }
+        public DateTime Return { get; set; }
+    }
+}
